980ce7ccf29f910a499c244b427667cd32f5b50e
saidmaharramli <saidmaharramlidev@gmail.com>
2026-05-24 08:02:14 +0400

    2nd commit

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -29,6 +29,7 @@ const errorMiddleware = (err, req, res, next) => {
         res.status(error.statusCode || 500).json({ success: false, error: error.message || 'Server Error' });
 
     } catch (error) {
+        next(error);
 
     }
 }
